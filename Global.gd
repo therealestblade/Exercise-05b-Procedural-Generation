e0b871ce10f6e75067d03f3ca00fffe5e3c39bfa
therealestblade <89621761+therealestblade@users.noreply.github.com>
2021-11-02 20:54:25 -0400

    Completes Exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,16 @@
 extends Node
 
+func _ready():
+	pause_mode = PAUSE_MODE_PROCESS
+
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
-		get_tree().quit()
+		#get_tree().quit()
+		var menu = get_node_or_null("/root/Game/Menu")
+		if menu != null:
+			if not menu.visible:
+				menu.show()
+				get_tree().paused = true
+			else:
+				menu.hide()
+				get_tree().paused = false
